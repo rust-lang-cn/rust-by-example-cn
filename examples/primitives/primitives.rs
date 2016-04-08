4fe175b998d6f24398806ffb751870202690a08e
Aaklo Xu <aakloxu@gmail.com>
2016-04-08 21:21:53 +0800

    examples/primitives/primitives.rs: Translate

--- examples/primitives/primitives.rs
+++ examples/primitives/primitives.rs
@@ -1,16 +1,16 @@
 fn main() {
-    // Variables can be type annotated.
+    // 变量可以声明类型。
     let logical: bool = true;
 
-    let a_float: f64 = 1.0;  // Regular annotation
-    let an_integer   = 5i32; // Suffix annotation
+    let a_float: f64 = 1.0;  // 常规声明
+    let an_integer   = 5i32; // 后缀声明
 
-    // Or a default will be used.
+    // 否则自动推断类型。
     let default_float   = 3.0; // `f64`
     let default_integer = 7;   // `i32`
 
-    let mut mutable = 12; // Mutable `i32`.
+    let mut mutable = 12; // 可变类型 `i32`。
 
-    // Error! The type of a variable can't be changed
+    // 错误！变量的类型不可改变。
     mutable = true;
 }
