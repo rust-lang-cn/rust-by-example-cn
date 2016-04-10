1c123fc0f202927da426d3bbfb17ebd351b27c1f
Aaklo Xu <aakloxu@gmail.com>
2016-04-10 23:08:15 +0800

    Translate: examples/primitives/tuples/input.md examples/primitives/tuples/tuples.rs

--- examples/primitives/tuples/input.md
+++ examples/primitives/tuples/input.md
@@ -1,28 +1,25 @@
-A tuple is a collection of values of different types. Tuples are constructed
-using parentheses `()`, and each tuple itself is a value with type signature
-`(T1, T2, ...)`, where `T1`, `T2` are the types of its members. Functions can
-use tuples to return multiple values, as tuples can hold any number of values.
+元组是一个可以包含各种类型的集合。元组使用括号 `()` 来构造，每个元组的值都是 `(T1, T2, ...)`
+这样的类型标记，其中 `T1`,`T2` 是每个成员的类型。函数可以使用元组来返回多个值，因为元组可以
+拥有任意数量的值。
 
 {tuples.play}
 
-### Activity
+### 动手试一试
 
- 1. *Recap*: Add the `fmt::Display` trait to the Matrix `struct` in the above example,
-    so that if you switch from printing the debug format `{:?}` to the display
-    format `{}`, you see the following output:
+ 1. *重新试一下*：在上面的例子中给 Matrix `结构体` 加上 `fmt::Display` trait，让你能够将
+    打印调试格式 `{:?}` 切换成一般显示格式 `{}`，得到如下的输出：
 ```
 ( 1.1 1.2 )
 ( 2.1 2.2 )
 ```
-    You may want to refer back to the example for [print display][print_display].
- 2. Add a `transpose` function using the `reverse` function as a template, which
-    accepts a matrix as an argument, and returns a matrix in which two elements
-    have been swapped. For example:
+    可以回顾之前学过的例子 [print display][print_display]。
+ 2. 以 `reverse` 函数作为样本，添加一个 `transpose` 函数，使它可以接受一个矩阵的参数，并
+    返回一个两个元素元素交换后矩阵。举个例子：
 ```
 println!("Matrix:\n{}", matrix)
 println!("Transpose:\n{}", transpose(matrix))
 ```
-results in the output:
+输出结果：
 ```
 Matrix:
 ( 1.1 1.2 )
@@ -32,4 +29,4 @@ Transpose:
 ( 1.2 2.2 )
 ```
 
-[print_display]: /hello/print/print_display.html
+[print_display]: ../hello/print/print_display.html
--- examples/primitives/tuples/tuples.rs
+++ examples/primitives/tuples/tuples.rs
@@ -1,30 +1,30 @@
-// Tuples can be used as function arguments and as return values
+// 元组可以充当函数的参数和返回值
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    // `let` can be used to bind the members of a tuple to variables
+    // 可以使用 `let` 来绑定元组的各个变量
     let (integer, boolean) = pair;
 
     (boolean, integer)
 }
 
-// The following struct is for the activity.
+// 在“动手试一试”的练习中要用到下面这个结构体。
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
 fn main() {
-    // A tuple with a bunch of different types
+    // 包含各种不同类型的元组
     let long_tuple = (1u8, 2u16, 3u32, 4u64,
                       -1i8, -2i16, -3i32, -4i64,
                       0.1f32, 0.2f64,
                       'a', true);
 
-    // Values can be extracted from the tuple using tuple indexing
+    // 通过元组的索引来访问具体的值
     println!("long tuple first value: {}", long_tuple.0);
     println!("long tuple second value: {}", long_tuple.1);
 
-    // Tuples can be tuple members
+    // 元组也可以充当元组的元素
     let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-    // Tuples are printable
+    // 元组可以打印
     println!("tuple of tuples: {:?}", tuple_of_tuples);
 
     let pair = (1, true);
@@ -32,12 +32,11 @@ fn main() {
 
     println!("the reversed pair is {:?}", reverse(pair));
 
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses
+    // 创建单元素元组需要一个额外的逗号，这是为了和括号包含的普通数据作区分。
     println!("one element tuple: {:?}", (5u32,));
     println!("just an integer: {:?}", (5u32));
 
-    //tuples can be destructured to create bindings
+    // 解构元组，将值赋给创建的绑定变量
     let tuple = (1, "hello", 4.5, true);
 
     let (a, b, c, d) = tuple;
