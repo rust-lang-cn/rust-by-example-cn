3fd98bd660b334a519932cc1b594da761b21bb32
Aaklo Xu <aakloxu@gmail.com>
2016-04-14 18:24:44 +0800

    Translate: examples/variable_bindings/input.md examples/variable_bindings/variable_bindings.rs

--- examples/variable_bindings/input.md
+++ examples/variable_bindings/input.md
@@ -1,8 +1,6 @@
-Rust provides type safety via static typing. Variable bindings can be type
-annotated when declared. However, in most cases, the compiler will be able
-to infer the type of the variable from the context, heavily reducing the
-annotation burden.
+Rust 通过静态类型确保类型安全。变量绑定可以在声明变量时标注类型。不过在多数情况下，编译器能够
+从字面内容推导出变量的类型，大大减少了标注类型的负担。
 
-Values (like literals) can be bound to variables, using the `let` binding.
+使用 `let` 绑定操作可以将值（像具体数据）绑定到变量中。
 
 {variable_bindings.play}
--- examples/variable_bindings/variable_bindings.rs
+++ examples/variable_bindings/variable_bindings.rs
@@ -3,17 +3,16 @@ fn main() {
     let a_boolean = true;
     let unit = ();
 
-    // copy `an_integer` into `copied_integer`
+    // 将 `an_integer` 复制到 `copied_integer`
     let copied_integer = an_integer;
 
     println!("An integer: {:?}", copied_integer);
     println!("A boolean: {:?}", a_boolean);
     println!("Meet the unit value: {:?}", unit);
 
-    // The compiler warns about unused variable bindings; these warnings can
-    // be silenced by prefixing the variable name with an underscore
+    // 编译器会对未使用变量绑定产生警告；可在变量名加上下划线的前缀来消除这些警告内容。
     let _unused_variable = 3u32;
 
     let noisy_unused_variable = 2u32;
-    // FIXME ^ Prefix with an underscore to suppress the warning
+    // 改正 ^ 在变量名加上下划线前缀消除警告
 }
