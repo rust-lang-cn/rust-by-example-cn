8458c515593d7a52161cd2964ac5aa9fc37df8a9
Aaklo Xu <aakloxu@gmail.com>
2016-04-17 18:46:18 +0800

    Translate: examples/cast/inference/inference.rs examples/cast/inference/input.md

--- examples/cast/inference/inference.rs
+++ examples/cast/inference/inference.rs
@@ -1,16 +1,15 @@
 fn main() {
-    // Because of the annotation, the compiler knows that `elem` has type u8.
+    // 借助类型标注，编译器知道 `elem` 具有 u8 类型。
     let elem = 5u8;
 
-    // Create an empty vector (a growable array).
+    // 创建一个空 vector（可增长数组）。
     let mut vec = Vec::new();
-    // At this point the compiler doesn't know the exact type of `vec`, it
-    // just knows that it's a vector of something (`Vec<_>`).
+    // 此时编译器并未知道 `vec` 的确切类型，它只知道 `vec` 是一个含有某种类型的 vector（`Vec<_>`）。
 
-    // Insert `elem` in the vector.
+    // 将 `elem` 插入 vector。
     vec.push(elem);
-    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
-    // TODO ^ Try commenting out the `vec.push(elem)` line
+    // Aha！现在编译器就知道了 `vec` 是一个含有 `u8` 类型的 vector(`Vec<u8>`)
+    // 试一试 ^ 尝试将 `vec.push(elem)` 那行注释掉
 
     println!("{:?}", vec);
 }
--- examples/cast/inference/input.md
+++ examples/cast/inference/input.md
@@ -1,12 +1,9 @@
-The type inference engine is pretty smart. It does more than looking at the
-type of the
-[r-value][rvalue]
-during an initialization. It also looks at how the variable is used afterwards 
-to infer its type. Here's an advanced example of type inference:
+类型推导引擎是相当智能的。它不仅仅在初始化期间分析[右值][rvalue]的类型，还会通过分析变量在后面是
+怎么使用的来推导该变量的类型。这里给出一个类型推导的高级例子：
 
 {inference.play}
 
-No type annotation of variables was needed, the compiler is happy and so is the
-programmer!
+无需变量的类型标注，编译器和程序员都很开心（the compiler is happy and so is the
+programmer）！
 
 [rvalue]: https://en.wikipedia.org/wiki/Value_%28computer_science%29#lrvalue
