5b606ba1755efea89d0967360541dca328333d3a
Aaklo Xu <aakloxu@gmail.com>
2016-05-04 23:30:08 +0800

    Translate: examples/fn/hof/hof.rs examples/fn/hof/input.md

--- examples/fn/hof/hof.rs
+++ examples/fn/hof/hof.rs
@@ -6,29 +6,29 @@ fn main() {
     println!("Find the sum of all the squared odd numbers under 1000");
     let upper = 1000;
 
-    // Imperative approach
-    // Declare accumulator variable
+    // 命令式方式（imperative approach）
+    // 声明累加器变量
     let mut acc = 0;
-    // Iterate: 0, 1, 2, ... to infinity
+    // 重复：0，1, 2, ... 到无穷大
     for n in 0.. {
-        // Square the number
+        // 数字的平方
         let n_squared = n * n;
 
         if n_squared >= upper {
-            // Break loop if exceeded the upper limit
+            // 若大于上限（upper limit）则退出循环
             break;
         } else if is_odd(n_squared) {
-            // Accumulate value, if it's odd
+            // 如果是奇数就累加值
             acc += n_squared;
         }
     }
     println!("imperative style: {}", acc);
 
-    // Functional approach
+    // 功能性方式（functional approach）
     let sum_of_squared_odd_numbers: u32 =
-        (0..).map(|n| n * n)             // All natural numbers squared
-             .take_while(|&n| n < upper) // Below upper limit
-             .filter(|n| is_odd(*n))     // That are odd
-             .fold(0, |sum, i| sum + i); // Sum them
+        (0..).map(|n| n * n)             // 所有自然数的平方
+             .take_while(|&n| n < upper) // 小于上限
+             .filter(|n| is_odd(*n))     // 为奇数
+             .fold(0, |sum, i| sum + i); // 最后其后
     println!("functional style: {}", sum_of_squared_odd_numbers);
 }
--- examples/fn/hof/input.md
+++ examples/fn/hof/input.md
@@ -1,13 +1,8 @@
-Rust provides Higher Order Functions (HOF). These are functions that
-take one or more functions and/or produce a more useful function. HOFs
-and lazy iterators give Rust its functional flavor.
-
+Rust 提供了高阶函数（Higher Order Function, HOF）。执行一个或多个函数来产生一个用处更大的函数。HOF 和惰性迭代器（lazy iterator）给 Rust 带来了功能性的风格（英文原文：HOFs 
+and lazy iterators give Rust its functional flavor.）。
 {hof.play}
 
-[Option][option]
-and
-[Iterator][iter]
-implement their fair share of HOFs.
+[Option][option] 和 [迭代器][iter] 实现了它们自己的高阶函数（英语原文：Option and Iterator implement their fair share of HOFs.）。
 
 [option]: http://doc.rust-lang.org/core/option/enum.Option.html
 [iter]: http://doc.rust-lang.org/core/iter/trait.Iterator.html
