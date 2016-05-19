6c1d2f578ff06950607219bd377570438aa2f351
Aaklo Xu <aakloxu@gmail.com>
2016-05-19 23:37:53 +0800

    Merge the official Rust by Example(English) to the Chinese Version

--- examples/cast/cast.rs
+++ examples/cast/cast.rs
@@ -21,8 +21,9 @@ fn main() {
     println!("1000 as a u16 is: {}", 1000 as u16);
 
     // 1000 - 256 - 256 - 256 = 232
-    // 在计算机底层会截取数字的低8位（the least significant bit，LSB），而高位（the most
-    // significant bit，MSB）数字会被抛掉（译注：此操作是按二进数存储的数字位进行）。
+    // 在计算机底层会截取数字的低8位（the least significant bit，LSB），
+    // 而高位（the most significant bit，MSB）数字会被抛掉。
+    // （译注：此操作是按二进数存储的数字位进行）
     println!("1000 as a u8 is : {}", 1000 as u8);
     // -1 + 256 = 255
     println!("  -1 as a u8 is : {}", (-1i8) as u8);
@@ -30,8 +31,10 @@ fn main() {
     // 对正数来说，上面的类型转换操作和取模效果一样
     println!("1000 mod 256 is : {}", 1000 % 256);
 
-    // 当将整数值转成有符号类型（signed 类型）时，一样要先将数值转成相应的无符号类型（译注：
-    // 如 i32 和 u32 对应，i16 和 u16对应），然后再求此值的补码（two's complement）
+    // 当将整数值转成有符号类型（signed 类型）时，同样要先将（二进制）数值
+    // 转成相应的无符号类型（译注：如 i32 和 u32 对应，i16 和 u16对应），
+    // 然后再求此值的补码（two's complement）。如果数值的最高位是 1，则数值
+    // 是负数。
 
     // 除非值本来就已经符合所要转的类型。
     println!(" 128 as a i16 is: {}", 128 as i16);
--- examples/custom_types/structs/input.md
+++ examples/custom_types/structs/input.md
@@ -6,9 +6,14 @@
 
 {structs.play}
 
+### 动手试一试：
+
+1. 增加一个计算长方形面积的函数 `rect_area`（尝试使用嵌套的解构方式）。
+2. 增加一个函数 `square`，接受的参数是一个 `Point` 和一个 `f32`，并返回一个 `Rectangle`（长方形）的信息，包括左下角的点，以及长和宽的浮点数值。
+
 ### 参见：
 
-[`attributes`][attributes] 和 [destructuring][destructuring]
+[`attributes`][attributes] 和 [解构][destructuring]
 
 [attributes]: ../attribute.html
 [c_struct]: http://en.wikipedia.org/wiki/Struct_(C_programming_language)
