f455f9d4a27d06f79cc0322375d0ff620afe0b2d
Aaklo Xu <aakloxu@gmail.com>
2016-06-05 00:27:59 +0800

    Translate: examples/scope/lifetime/lifetime_coercion/coercion.rs examples/scope/lifetime/lifetime_coercion/input.md

--- examples/scope/lifetime/lifetime_coercion/coercion.rs
+++ examples/scope/lifetime/lifetime_coercion/coercion.rs
@@ -1,20 +1,21 @@
-// Here, Rust infers a lifetime that is as short as possible.
-// The two references are then coerced to that lifetime.
+// 在这里，Rust 推导了一个尽可能短的生命周期。
+// 然后这两个引用都被强制转成这个生命周期。
 fn multiply<'a>(first: &'a i32, second: &'a i32) -> i32 {
     first * second
 }
 
-// `<'a: 'b, 'b>` reads as lifetime `'a` is at least as long as `'b`.
-// Here, we take in an `&'a i32` and return a `&'b i32` as a result of coercion.
+// `<'a: 'b, 'b>` 理解为生命周期 `'a` 至少和 `'b` 一样长。
+// 在这里我们我们接受了一个 `&'a i32` 类型并返回一个 `&'b i32` 类型，这是
+// 强制转换得到的结果。
 fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
     first
 }
 
 fn main() {
-    let first = 2; // Longer lifetime
+    let first = 2; // 较长的生命周期
     
     {
-        let second = 3; // Shorter lifetime
+        let second = 3; // 较短的生命周期
         
         println!("The product is {}", multiply(&first, &second));
         println!("{} is the first", choose_first(&first, &second));
--- examples/scope/lifetime/lifetime_coercion/input.md
+++ examples/scope/lifetime/lifetime_coercion/input.md
@@ -1,6 +1,4 @@
-A longer lifetime can be coerced into a shorter one 
-so that it works inside a scope it normally wouldn't work in.
-This comes in the form of inferred coercion by the Rust compiler,
-and also in the form of declaring a lifetime difference:
+一个较长的生命周期可以强制转成一个较短的生命周期，使它在一个通常情况下不能工作的作用域内也能正常工作。这种形式出现在编译器推导强制转换的时候，也出现在声明生命周期不同的时候（原文：This comes in the form of inferred coercion by the Rust compiler,
+and also in the form of declaring a lifetime difference）：
 
 {coercion.play}
