99d1a50c778569b0e3ca9142a3692c71bbd765cd
Aaklo Xu <aakloxu@gmail.com>
2016-06-05 08:12:34 +0800

    Translate: examples/scope/lifetime/static_lifetime/input.md examples/scope/lifetime/static_lifetime/static_lifetime.rs

--- examples/scope/lifetime/static_lifetime/input.md
+++ examples/scope/lifetime/static_lifetime/input.md
@@ -1,18 +1,14 @@
-A `'static` lifetime is longest possible lifetime, and lasts for 
-the lifetime of the running program. A `'static` lifetime may also be 
-coerced to a shorter lifetimes. There are two ways to make a variable 
-with `'static` lifetime, and both are stored in the read-only memory
-of the binary:
+`'static` 生命周期在可能存在的生命周期中是最长的，并在运行程序的周期中持续存在。`static` 生命周期也可能被强制转换成一个更短的生命周期。有两种方式使变量拥有 `static` 生命周期，这两种方式都是保存在可执行文件的只读内存区：
 
-* Make a constant with the `static` declaration.
-* Make a `string` literal which has type: `&'static str`.
+* 使用 `static` 声明来产生常量（constant）。
+* 产生一个拥有 `&'static str` 类型的 `string` 字面量。
 
-See the following example for a display of each method:
+看下面的例子，了解列举到的各个方法：
 
 {static_lifetime.play}
 
-### See also:
+### 参见：
 
-[`'static` constants][static_const]
+[`'static` 常量][static_const]
 
-[static_const]: /custom_types/constants.html
+[static_const]: ../../custom_types/constants.html
--- examples/scope/lifetime/static_lifetime/static_lifetime.rs
+++ examples/scope/lifetime/static_lifetime/static_lifetime.rs
@@ -1,27 +1,27 @@
-// Make a constant with `'static` lifetime.
+// 产生一个拥有 `'static` 生命周期的常量。
 static NUM: i32 = 18;
 
-// Returns a reference to `NUM` where its `'static` 
-// lifetime is coerced to that of the input argument.
+// 返回一个指向 `NUM` 的引用，其中`NUM` 的 `'static`
+// 生命周期被强制转换成和输入参数的一样。
 fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
     &NUM
 }
 
 fn main() {
     {
-        // Make a `string` literal and print it:
+        // 产生一个 `string` 字面量并打印它：
         let static_string = "I'm in read-only memory";
         println!("static_string: {}", static_string);
 
-        // When `static_string` goes out of scope, the reference
-        // can no longer be used, but the data remains in the binary.
+        // 当 `static_string` 离开作用域时，该引用不能再使用，不过
+        // 数据会保留在二进制文件里面。
     }
     
     {
-        // Make an integer to use for `coerce_static`:
+        // 产生一个整型给 `coerce_static` 使用：
         let lifetime_num = 9;
 
-        // Coerce `NUM` to lifetime of `lifetime_num`:
+        // 将 `NUM` 强制转换成 `lifetime_num` 的生命周期：
         let coerced_static = coerce_static(&lifetime_num);
 
         println!("coerced_static: {}", coerced_static);
