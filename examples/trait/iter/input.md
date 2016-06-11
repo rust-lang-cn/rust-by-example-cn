b8f815ab86dad50d3926cb17f452b976c23bdebc
Aaklo Xu <aakloxu@gmail.com>
2016-06-12 00:37:22 +0800

    Translate: examples/trait/iter/input.md examples/trait/iter/iter.rs

--- examples/trait/iter/input.md
+++ examples/trait/iter/input.md
@@ -1,14 +1,10 @@
-The `Iterator` trait is used to implement iterators over collections such as arrays.
+`Iterator` trait 用来实现关于集合（collection）类型（比如数组）的迭代器。
 
-The trait requires only a method to be defined for the `next` element, 
-which may be manually defined in an `impl` block or automatically 
-defined (as in arrays and ranges).
+这个 trait 只需定义一个指向 `next`（下一个）元素的方法，这可手动在 `impl` 代码块中定义，或者自动定义（比如在数组或区间中）。
 
-As a point of convenience for common situations, the `for` construct 
-turns some collections into iterators using the [`.into_iterator()`][intoiter] method.
+为方便起见，`for` 结构通常使用 [`.into_iterator()`][intoiter] 方法将一些集合类型转换为迭代器。
 
-Methods that can be accessed using the `Iterator` trait in addition 
-to those shown in the example below can be found [here][iter].
+下面例子展示了如何访问使用 `Iterator` trait 的方法，关于这方面的更多内容可[点击这里][iter]查看。
 
 {iter.play}
 
--- examples/trait/iter/iter.rs
+++ examples/trait/iter/iter.rs
@@ -3,34 +3,34 @@ struct Fibonacci {
     next: u32,
 }
 
-// Implement `Iterator` for `Fibonacci`.
-// The `Iterator` trait only requires a method to be defined for the `next` element.
+// 实现关于 `Fibonacci` （斐波那契）的 `Iterator`。
+// `Iterator` trait 只需定义一个指向 `next`（下一个）元素的方法。
 impl Iterator for Fibonacci {
     type Item = u32;
     
-    // Here, we define the sequence using `.curr` and `.next`.
-    // The return type is `Option<T>`:
-    //     * When the `Iterator` is finished, `None` is returned.
-    //     * Otherwise, the next value is wrapped in `Some` and returned.
+    // 我们在这里使用 `.curr` 和 `.next` 来定义数列（sequence）。
+    // 返回类型为 `Option<T>`：
+    //     * 当 `Iterator` 结束时，返回 `None`。
+    //     * 其他情况，返回被 `Some` 包裹（wrapped）的下一个值。
     fn next(&mut self) -> Option<u32> {
         let new_next = self.curr + self.next;
 
         self.curr = self.next;
         self.next = new_next;
 
-        // Since there's no endpoint to a Fibonacci sequence, the `Iterator` 
-        // will never return `None`, and `Some` is always returned.
+        // 既然斐波那契数列不存在不存在终点，那么 `Iterator` 将不可能
+        // 返回 `None`，而总是返回 `Some`。
         Some(self.curr)
     }
 }
 
-// Returns a Fibonacci sequence generator
+// 返回一个斐波那契数列生存器（generator）
 fn fibonacci() -> Fibonacci {
     Fibonacci { curr: 1, next: 1 }
 }
 
 fn main() {
-    // `0..3` is an `Iterator` that generates: 0, 1, and 2.
+    // `0..3` 是一个 `Iterator`，会产生：0，1 和 2。
     let mut sequence = 0..3;
 
     println!("Four consecutive `next` calls on 0..3");
@@ -39,20 +39,20 @@ fn main() {
     println!("> {:?}", sequence.next());
     println!("> {:?}", sequence.next());
 
-    // `for` works through an `Iterator` until it returns `None`.
-    // Each `Some` value is unwrapped and bound to a variable (here, `i`).
+    // `for` 通过 `Iterator` 进行工作，直到 `Iterator` 为 `None`。
+    // 每个 `Some` 值都被解包（unwrap）且限定为一个变量（这里是 `i`）。
     println!("Iterate through 0..3 using `for`");
     for i in 0..3 {
         println!("> {}", i);
     }
 
-    // The `take(n)` method reduces an `Iterator` to its first `n` terms.
+    // `take(n)` 方法提取 `Iterator` 的前 `n` 项。
     println!("The first four terms of the Fibonacci sequence are: ");
     for i in fibonacci().take(4) {
         println!("> {}", i);
     }
 
-    // The `skip(n)` method shortens an `Iterator` by dropping its first `n` terms.
+    // `skip(n)` 方法通过跳过前 `n` 项缩短了 `Iterator` 。
     println!("The next four terms of the Fibonacci sequence are: ");
     for i in fibonacci().skip(4).take(4) {
         println!("> {}", i);
@@ -60,7 +60,7 @@ fn main() {
 
     let array = [1u32, 3, 3, 7];
 
-    // The `iter` method produces an `Iterator` over an array/slice.
+    // `iter` 方法对数组/slice 产生一个 `Iterator`。
     println!("Iterate the following array {:?}", &array);
     for i in array.iter() {
         println!("> {}", i);
