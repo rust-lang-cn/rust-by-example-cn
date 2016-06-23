c60f38f57834904859810d213df0db8c65f1db7b
Aaklo Xu <aakloxu@gmail.com>
2016-06-23 23:16:22 +0800

    Translate: examples/error/option_with_result/result_string_errors/input.md examples/error/option_with_result/result_string_errors/result_string.rs

--- examples/error/option_with_result/result_string_errors/input.md
+++ examples/error/option_with_result/result_string_errors/input.md
@@ -1,11 +1,7 @@
-From our previous example, one method of solving our issue with `unwrap` is to remove it.
-In doing so, we must move from implicit to explicit error handling. Since the only 
-types in play are `Option` and `Result`, we can consider converting both into 
-`Result`s with the same `Err` type. For our first attempt at this solution, 
-let's try using a `String` for our error:
+从前面例子可以看到，一个解决这个带有 `unwrap` 的问题的方法就是将 `unwrap` 移除。为达成这点，我们必须由隐式的错误处理转examples/error/option_with_result/input.md
+	modified:   examples/error/option_with_result/option_result.rs成显式的错误处理。既然运行中的类型只有 `Option` 和 `Result`，我们可以考虑将两者转换成带有相同 `Err` 类型的 `Result`。作为问题解决的第一次尝试，让我们试试使用字符串（`String`）来应对错误（原文：For our first attempt at this solution, 
+let's try using a `String` for our error）：
 
 {result_string.play}
 
-This is not too bad, but it is hardly as nice as the original (it can still be nicer but
-we are not there yet). Unfortunately, this approach scales poorly with increasing 
-numbers of `Result`s, as will be seen in the next example.
+情况不算特别糟糕，但相比原始代码很难说足够优雅（当然也可以改得更优雅，但这不是我们要关心的重点）。悲剧的是，这种方法通过枯燥地增加更多的 `Result` 来达到目的，这种情况在下个例子中也会看到（原文：Unfortunately, this approach scales poorly with increasing numbers of `Result`s, as will be seen in the next example.）。
--- examples/error/option_with_result/result_string_errors/result_string.rs
+++ examples/error/option_with_result/result_string_errors/result_string.rs
@@ -1,17 +1,19 @@
-// Use `String` as our error type
+// 使用 `String` 作为错误类型
 type Result<T> = std::result::Result<T, String>;
 
 fn double_first(vec: Vec<&str>) -> Result<i32> {
     vec.first()
-       // Convert the `Option` to a `Result` if there is a value.
-       // Otherwise, provide an `Err` containing this `String`.
+       // 若值存在则将 `Option` 转换成 `Result`。
+       // 否则提供一个包含该字符串（`String`） 的 `Err`。
        .ok_or("Please use a vector with at least one element.".to_owned())
-       // Recall that `parse` returns a `Result<T, ParseIntError>`.
+       // 回想一下，`parse` 返回一个 `Result<T, ParseIntError>`。
        .and_then(|s| s.parse::<i32>()
-                      // Map the errors `parse` yields to `String`.
-                      // The return type is then `Result<T, String>`.
+                      // 映射错误 `parse` 产生得到 `String`。
+                      // 然后返回类型变成 `Result<T, String>`。
+                      // （原文：Map the errors `parse` yields to `String`.
+                      // The return type is then `Result<T, String>`.）
                       .map_err(|e| e.to_string())
-                      // At this point, we can double the number inside.
+                      // 根据这点，我们可以推断出里面的数字。
                       .map(|i| 2 * i))
 }
 
