26551806cb1059a9b9e901adfa795fbb40cae645
Aaklo Xu <aakloxu@gmail.com>
2016-07-02 23:41:49 +0800

    Translate: examples/error/limits_of_strings/input.md examples/error/limits_of_strings/rethink.rs

--- examples/error/limits_of_strings/input.md
+++ examples/error/limits_of_strings/input.md
@@ -1,27 +1,22 @@
-We have been using `Strings` as errors for a while. In fact, this is somewhat limiting as
-an error type. Below are the criteria for a good error type. `String` nicely fulfills the first
-two but not the second two:
+前面我们一直使用字符串（`String`）作为错误消息。实际上，字符串作为错误类型是存在一些局限的。下面是友好的错误类型标准。字符串（`String`）很好地实现了前两点，但无法做到后两点：
 
-* Represents different errors with the same type
-* Presents nice error messages to the user
-* Is easily type comparable. Consider comparing these two types:
+* 使用相同类型来表达不同的错误
+* 给用户提供友好的错误信息
+* 支持良好的错误比较。考虑比较以下两种类型：
     - `Err("Please use a vector with at least one element".to_owned())`
     - `Err(EmptyVec)`
-* Can hold information about the error. Compare:
+* 能够保存错误的信息（原文：Can hold information about the error.）。比较：
     - `Err("+ cannot be used here".to_owned())`
     - `Err(BadChar(c, position))`
 
-This makes `String` errors both difficult to react to and verbose to create. In fact, a nice
-looking error message has nothing to do with how the type is structured. It is simply a
-consequence of `Display` being implemented for the type. It should not be necessary to
-pollute logic heavy code with `String` formatting simply for nice error messages.
+这使得 `String` 错误既难以达到要求，也难以创建。实际上，一个优雅的错误消息应该和类型组织的形式是无关的。这只不过是 `Display` 对类型实现的结果。对优雅的错误消息来说，实在不应该使用 `String` 格式化方式，因为这会严重污染代码的逻辑（原文：It should not be necessary to pollute logic heavy code with `String` formatting simply for nice error messages.）。
 
 {rethink.play}
 
-### See also:
+### 参见：
 
-[`Result`][result] and [`io::Result`][io_result]
+[`Result`][result] 和 [`io::Result`][io_result]
 
 [result]: http://doc.rust-lang.org/std/result/enum.Result.html
 [io_result]: http://doc.rust-lang.org/std/io/type.Result.html
-[inplace]: /error/option_with_result/result_string_errors.html
+[inplace]: ../error/option_with_result/result_string_errors.html
--- examples/error/limits_of_strings/rethink.rs
+++ examples/error/limits_of_strings/rethink.rs
@@ -4,31 +4,32 @@ use std::fmt;
 type Result<T> = std::result::Result<T, DoubleError>;
 
 #[derive(Debug)]
-// Define our error types. These may be customized however is useful for our error
+// 定义我们的错误类型。不管对我们的错误处理情况有多重要，这些都可能自定义。
+// 现在我们能够按照底层工具的错误实现，写下我们的错误，或者两者之间的内容。
+// （原文：Define our error types. These may be customized however is useful for our error
 // handling cases. Now we will be able to defer to the underlying tools error
-// implementation, write our own errors, or something in between.
+// implementation, write our own errors, or something in between.）
 enum DoubleError {
-    // We don't require any extra info to detail this error.
+    // 我们不需要任何额外的信息来描述这个错误。
     EmptyVec,
-    // We will defer to the parse error implementation for their error. Supplying extra
-    // info would require adding more data to the type.
+    // 我们将推迟对于这些错误的解析错误的实现。（原文：We will defer to the parse
+    // error implementation for their error.）提供额外信息将要增加更多针对类型的数据。
     Parse(ParseIntError),
 }
 
-// How the type is displayed is completely separate from where the errors are generated.
-// We do not need to be concerned that the display style will clutter the complex logic
-// our utility requires. They are separate matters which are handled separately.
+// 类型的展示方式的和类型的产生方式是完全独立的。我们无需担心显示样式会搞乱我们
+// 工具集所需的复杂逻辑。它们是独立的，就是说它们处理起来是相互独立的。
 //
-// We don't store extra info about the errors. If we had desired, for example, to state
-// which string failed to parse then we can't without modifying our types to carry that
-// information accordingly.
+// 我们没有存储关于错误的额外信息。若确实想要，比如，要指出哪个字符串无法解析，
+// 那么我们不得不修改我们类型来携带相应的信息。
 impl fmt::Display for DoubleError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             DoubleError::EmptyVec =>
                 write!(f, "please use a vector with at least one element"),
-            // This is a wrapper so defer to the underlying types' own implementation
-            // of `fmt`.
+            // 这是一个 wrapper，所以按照底层类型来给出我们的 `fmt` 实现。
+            // （原上：This is a wrapper so defer to the underlying types' own implementation
+            // of `fmt`.）
             DoubleError::Parse(ref e) => e.fmt(f),
         }
     }
@@ -36,10 +37,10 @@ impl fmt::Display for DoubleError {
 
 fn double_first(vec: Vec<&str>) -> Result<i32> {
     vec.first()
-       // Change the error to our new type.
+       // 将错误改成我们新的类型。
        .ok_or(DoubleError::EmptyVec)
        .and_then(|s| s.parse::<i32>()
-            // Update to the new error type here also.
+             // 在这里也更新成新的错误类型。    
             .map_err(DoubleError::Parse)
             .map(|i| 2 * i))
 }
