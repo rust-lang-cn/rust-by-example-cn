2b6b6ce1f124623f7a5b3dfdbe1da6c000233d22
Aaklo Xu <aakloxu@gmail.com>
2016-07-11 23:30:04 +0800

    Translate: examples/std/str/str.rs

--- examples/std/str/str.rs
+++ examples/std/str/str.rs
@@ -1,38 +1,38 @@
 fn main() {
-    // (all the type annotations are superfluous)
-    // A reference to a string allocated in read only memory
+    // （所有的类型标注都是都是多余）
+    // 一个指向在只读内存中堆分配字符串的引用
     let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
     println!("Pangram: {}", pangram);
 
-    // Iterate over words in reverse, no new string is allocated
+    // 逆序迭代单词，不用分配新的字符串
+    // （原文：Iterate over words in reverse, no new string is allocated）    
     println!("Words in reverse");
     for word in pangram.split_whitespace().rev() {
         println!("> {}", word);
     }
 
-    // Copy chars into a vector, sort and remove duplicates
+    // 复制字符到一个 vector，排序并移除重复值
     let mut chars: Vec<char> = pangram.chars().collect();
     chars.sort();
     chars.dedup();
 
-    // Create an empty and growable `String`
+    // 创建一个空的且可增长的 `String`
     let mut string = String::new();
     for c in chars {
-        // Insert a char at the end of string
+        // 在字符串的尾部插入一个字符
         string.push(c);
-        // Insert a string at the end of string
+        // 在字符串尾部插入一个字符串
         string.push_str(", ");
     }
 
-    // The trimmed string is a slice to the original string, hence no new
-    // allocation is performed
+    // 此切割的字符串是原字符串的一个切片，所以没有执行新分配操作
     let chars_to_trim: &[char] = &[' ', ','];
     let trimmed_str: &str = string.trim_matches(chars_to_trim);
     println!("Used characters: {}", trimmed_str);
 
-    // Heap allocate a string
+    // 堆分配一个字符串
     let alice = String::from("I like dogs");
-    // Allocate new memory and store the modified string there
+    // 分配新内存并存储修改过的字符串
     let bob: String = alice.replace("dog", "cat");
 
     println!("Alice says: {}", alice);
