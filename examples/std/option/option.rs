7cd60ef3e165f7597174c2322576fd68a9ba771f
Aaklo Xu <aakloxu@gmail.com>
2016-07-12 23:54:19 +0800

    Translate: examples/std/option/option.rs

--- examples/std/option/option.rs
+++ examples/std/option/option.rs
@@ -1,17 +1,17 @@
-// An integer division that doesn't `panic!`
+// 不会 `panic!` 的整数除法。
 fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
     if divisor == 0 {
-        // Failure is represented as the `None` variant
+        // 失败表示成 `None` 变量
         None
     } else {
-        // Result is wrapped in a `Some` variant
+        // 结果 Result 被装包成 `Some` 变量
         Some(dividend / divisor)
     }
 }
 
-// This function handles a division that may not succeed
+// 此函数处理可能失败的除法
 fn try_division(dividend: i32, divisor: i32) {
-    // `Option` values can be pattern matched, just like other enums
+    // `Option` 值可以进行模式匹配，就和其他枚举一样
     match checked_division(dividend, divisor) {
         None => println!("{} / {} failed!", dividend, divisor),
         Some(quotient) => {
@@ -24,15 +24,16 @@ fn main() {
     try_division(4, 2);
     try_division(1, 0);
 
-    // Binding `None` to a variable needs to be type annotated
+    // 绑定 `None` 到一个变量需要类型标注
     let none: Option<i32> = None;
     let _equivalent_none = None::<i32>;
 
     let optional_float = Some(0f32);
 
-    // Unwrapping a `Some` variant will extract the value wrapped.
+    // 解包 `Some` 变量将展开解包后的值。
+    // （原文：Unwrapping a `Some` variant will extract the value wrapped.）
     println!("{:?} unwraps to {:?}", optional_float, optional_float.unwrap());
 
-    // Unwrapping a `None` variant will `panic!`
+    // 解包 `None` 变量将会引发 `panic!`。
     println!("{:?} unwraps to {:?}", none, none.unwrap());
 }
