01a04ca590b9f7dca977b8d528f0206dd3ad8f18
Aaklo Xu <aakloxu@gmail.com>
2016-07-14 23:21:22 +0800

    Translate: examples/std/panic/panic.rs

--- examples/std/panic/panic.rs
+++ examples/std/panic/panic.rs
@@ -1,22 +1,22 @@
-// Re-implementation of integer division (/)
+// 再次实现整型的除法（/）
 fn division(dividend: i32, divisor: i32) -> i32 {
     if divisor == 0 {
-        // Division by zero triggers a panic
+        // 除以一个 0 时会引发一个 panic
         panic!("division by zero");
     } else {
         dividend / divisor
     }
 }
 
-// The `main` task
+// `main` 任务
 fn main() {
-    // Heap allocated integer
+    // 堆分配的整数
     let _x = Box::new(0i32);
 
-    // This operation will trigger a task failure
+    // 此操作将会引发一个任务失败
     division(3, 0);
 
     println!("This point won't be reached!");
 
-    // `_x` should get destroyed at this point
+    // `_x` 在此处将被销毁
 }
