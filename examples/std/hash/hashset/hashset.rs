af0577eddd60a64e292c18550ed176eb892073df
Aaklo Xu <aakloxu@gmail.com>
2016-07-23 23:34:20 +0800

    Translate: examples/std/hash/hashset/hashset.rs

--- examples/std/hash/hashset/hashset.rs
+++ examples/std/hash/hashset/hashset.rs
@@ -7,29 +7,27 @@ fn main() {
     assert!(a.insert(4));
     assert!(a.contains(&4));
 
-    // `HashSet::insert()` returns false if
-    // there was a value already present.
+    // 如果值已经存在，那么 `HashSet::insert()` 返回 false。
     assert!(b.insert(4), "Value 4 is already in set B!");
-    // FIXME ^ Comment out this line
+    // 改正 ^ 将此行注释掉。
 
     b.insert(5);
 
-    // If a collection's element type implements `Debug`,
-    // then the collection implements `Debug`.
-    // It usually prints its elements in the format `[elem1, elem2, ...]`
+    // 若一个组合的元素类型实现了 `Debug`，那么该组合也就实现了 `Debug`。
+    // 这通常将元素打印成这样的格式 `[dlem1, elem2, ...]
     println!("A: {:?}", a);
     println!("B: {:?}", b);
 
-    // Print [1, 2, 3, 4, 5] in arbitrary order
+    // 乱序打印 [1, 2, 3, 4, 5]。
     println!("Union: {:?}", a.union(&b).collect::<Vec<&i32>>());
 
-    // This should print [1]
+    // 这将会打印出 [1]
     println!("Difference: {:?}", a.difference(&b).collect::<Vec<&i32>>());
 
-    // Print [2, 3, 4] in arbitrary order.
+    // 乱序打印 [2, 3, 4]。
     println!("Intersection: {:?}", a.intersection(&b).collect::<Vec<&i32>>());
 
-    // Print [1, 5]
+    // 打印 [1, 5]
     println!("Symmetric Difference: {:?}",
              a.symmetric_difference(&b).collect::<Vec<&i32>>());
 }
