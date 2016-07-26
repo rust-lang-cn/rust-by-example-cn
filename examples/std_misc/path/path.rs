b08798de5180dd68944e0d6fc3443d18b7a85e02
Aaklo Xu <aakloxu@gmail.com>
2016-07-26 23:49:14 +0800

    Translate examples/std_misc/path/path.rs

--- examples/std_misc/path/path.rs
+++ examples/std_misc/path/path.rs
@@ -1,17 +1,16 @@
 use std::path::Path;
 
 fn main() {
-    // Create a `Path` from an `&'static str`
+    // 从 `&'static str` 创建一个 `Path`
     let path = Path::new(".");
 
-    // The `display` method returns a `Show`able structure
+    // `display` 方法返回一个可显示（showable）的结构体
     let display = path.display();
 
-    // `join` merges a path with a byte container using the OS specific
-    // separator, and returns the new path
+    // `join` 使用操作系统的特定分隔符来合并路径，并返回新的路径
     let new_path = path.join("a").join("b");
 
-    // Convert the path into a string slice
+    // 将路径转换成一个字符串 slice
     match new_path.to_str() {
         None => panic!("new path is not a valid UTF-8 sequence"),
         Some(s) => println!("new path is {}", s),
