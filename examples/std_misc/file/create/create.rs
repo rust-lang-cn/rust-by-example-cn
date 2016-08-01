8b851036a049db453e00bf13c7dcef3691d633ed
Aaklo Xu <aakloxu@gmail.com>
2016-08-01 23:04:13 +0800

    Translate examples/std_misc/file/create/create.rs

--- examples/std_misc/file/create/create.rs
+++ examples/std_misc/file/create/create.rs
@@ -16,7 +16,7 @@ fn main() {
     let path = Path::new("out/lorem_ipsum.txt");
     let display = path.display();
 
-    // Open a file in write-only mode, returns `io::Result<File>`
+    // 以只写模式打开文件，返回 `io::Result<File>`
     let mut file = match File::create(&path) {
         Err(why) => panic!("couldn't create {}: {}",
                            display,
@@ -24,7 +24,7 @@ fn main() {
         Ok(file) => file,
     };
 
-    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    // 将 `LOREM_IPSUM` 字符串写进 `file`，返回 `io::Result<()>`
     match file.write_all(LOREM_IPSUM.as_bytes()) {
         Err(why) => {
             panic!("couldn't write to {}: {}", display,
