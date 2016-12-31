7d2b36663b4be92b90d3f5ec3781cbb5d69c59c2
Zhirui Li <zr.public@outlook.com>
2016-12-31 21:44:53 +0800

    改进翻译

--- examples/generics/generics.rs
+++ examples/generics/generics.rs
@@ -1,14 +1,14 @@
 // 具体的类型 `A`。
 struct A;
 
-// 在定义类型 `Single` 时，`A` 的首次使用之前没有给出 `<A>` 实现。
+// 在定义类型 `Single` 时，在 `A` 的首次使用之前没有出现 `<A>`。
 // 因此，`Single` 是一个具体的类型，`A` 在上面已经定义。
 // （原文：In defining the type `Single`, the first use of `A` is not preceded
 // by `<A>`. Therefore, `Single` is a concrete type, and `A` is defined as above.）
 struct Single(A);
 //            ^ 这里是 `Single` 对类型 `A` 的第一次使用。
 
-// 此处 `<T>` 在第一次使用 `T` 前给出，所以 `SingleGen` 是一个泛型类型。
+// 此处 `<T>` 在第一次使用 `T` 前出现，所以 `SingleGen` 是一个泛型类型。
 // 因为类型参量 `T` 是泛型，所以它可以是任何类型，包括在上面定义的具体类型 `A`。
 struct SingleGen<T>(T);
 
