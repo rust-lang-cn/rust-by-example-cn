989a8c6646af4a89e2a9b7acc215f3fb28629ca7
Aaklo Xu <aakloxu@gmail.com>
2017-01-05 23:13:10 +0800

    Use structs, not tuples, for generics impl example.——2016-8-19 Over IRC, had a user say they found the usage of tuple get syntax to be confusing (`self.0`) so this patch replaces the usage of that with more mundane syntax so that learners can focus on the impl syntax.

--- examples/generics/impl/impl.rs
+++ examples/generics/impl/impl.rs
@@ -1,19 +1,24 @@
-struct Val (f64,);
-struct GenVal<T>(T,);
+struct Val {
+    val: f64
+}
+
+struct GenVal<T>{
+    gen_val: T
+}
 
 // Val 的实现（impl）
 impl Val {
-    fn value(&self) -> &f64 { &self.0 }
+    fn value(&self) -> &f64 { &self.val }
 }
 
 // GenVal 针对泛型类型 `T` 的实现
 impl <T> GenVal<T> {
-    fn value(&self) -> &T { &self.0 }
+    fn value(&self) -> &T { &self.gen_val }
 }
 
 fn main() {
-    let x = Val(3.0);
-    let y = GenVal(3i32);
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
     
     println!("{}, {}", x.value(), y.value());
 }
