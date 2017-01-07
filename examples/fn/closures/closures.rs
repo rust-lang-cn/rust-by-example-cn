bef2fd5f835c288571ddde39360330f6d613460c
Aaklo Xu <aakloxu@gmail.com>
2017-01-07 21:56:59 +0800

    Remove print() closure

--- examples/fn/closures/closures.rs
+++ examples/fn/closures/closures.rs
@@ -17,13 +17,4 @@ fn main() {
     // 返回类型是自动推导的。
     let one = || 1;
     println!("closure returning one: {}", one());
-
-    // 从封闭的环境中捕获变量是可能的；而这行为对函数来说是不可能的。
-    let professor_x = "Charles Xavier";
-
-    // 下面闭包从封闭的环境中打印一个变量，没有参数和返回值。
-    let print = || println!("Professor X's name is: {}", professor_x);
-
-    // 调用闭包。
-    print();
 }
